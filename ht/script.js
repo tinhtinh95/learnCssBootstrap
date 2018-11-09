5087b898d7530db541e0d66f95113d6ca935f093
Tina <tina@enclave.vn>
2018-11-09 17:35:06 +0700

    learn Css & Bootstrap

---
+++ ht/script.js
@@ -0,0 +1,13 @@
+// Vue.component('signUpForm',{
+//     template: '<p>ddd<p>'
+// });
+
+
+
+// new Vue({
+//     el:'#app',
+//     data:{
+//         componentName: 'signUpForm',
+//         message: 'DDDD'
+//     }
+// })
