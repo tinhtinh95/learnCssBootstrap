21dee0c137d4ae28fa6348baf5e4c9778c6834f2
tinhtinh95 <nttinh995@gmail.com>
2018-11-23 00:08:14 +0700

    contact-form

--- angularPro/learnDirectives/src/app/app.module.ts
+++ angularPro/learnDirectives/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angularPro/learnDirectives/src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent {
+  contactMethods =[
+    {id: 1, name: 'Phone'},
+    {id: 2, name: 'Fax'},
+  ]
+
+  log(x){
+    console.log(x)
+  }
+
+  submit(f){
+    console.log(f);
+  }
+}
