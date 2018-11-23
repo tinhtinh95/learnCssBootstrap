b8739b278726f7d54d1ccc3a45f8bbf8392bee89
Tina <tina@enclave.vn>
2018-11-23 16:34:33 +0700

    http services

---
+++ angularPro/httpServices/src/app/post/post.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent {
+  posts: any[];
+  constructor(http: Http) {
+    http.get('https://jsonplaceholder.typicode.com/posts')
+      .subscribe(res => {
+        this.posts = res.json();
+      });
+  }
+}
