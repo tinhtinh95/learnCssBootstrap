052cd2d543dcc28faa491443a6b1d530799872fd
Tina <tina@enclave.vn>
2018-11-28 17:23:37 +0700

    Start OSHOP project

---
+++ angularPro/oshop/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from './../environments/environment';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BsNavbarComponent
+  ],
+  imports: [
+  BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
