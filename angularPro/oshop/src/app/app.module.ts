7dde355cf524e1d935836d1ac1fa187e712994c4
tinhtinh95 <nttinh995@gmail.com>
2018-11-28 23:55:49 +0700

    Deploy to firebase

--- angularPro/oshop/src/app/app.module.ts
+++ angularPro/oshop/src/app/app.module.ts
@@ -4,23 +4,54 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from './../environments/environment';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { from } from 'rxjs';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { LoginComponent } from './login/login.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BsNavbarComponent
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    MyOrdersComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    LoginComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent
   ],
   imports: [
   BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'products', component: ProductsComponent},
+      { path: 'shopping-cart', component: ShoppingCartComponent},
+      { path: 'check-out', component: CheckOutComponent},
+      { path: 'order-success', component: OrderSuccessComponent},
+      { path: 'my-orders', component: MyOrdersComponent},
+      { path: 'login', component: LoginComponent},
+      { path: 'admin/products', component: AdminProductsComponent}, 
+      { path: 'admin/orders', component: AdminOrdersComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
